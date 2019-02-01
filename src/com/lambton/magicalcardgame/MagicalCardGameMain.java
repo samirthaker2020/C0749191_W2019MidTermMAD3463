1a1fb509f48c89445c0fdf8b6b66f590ec4c8a15
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-31 20:35:16 -0500

    modified files in project

--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -2,14 +2,13 @@ package com.lambton.magicalcardgame;
 import java.util.*;
 public class MagicalCardGameMain {
 
-    public static void main (String args[])
-    {
+    public static void main (String args[]) {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
 
-        String [][]cards = {{"1","9","k"},{"A","7","3"},{"3","6","D"}};
+        String[][] cards = {{"1", "9", "k"}, {"A", "7", "3"}, {"3", "6", "D"}};
         System.out.println("Please Select Any One::");
-        for (int i=0;i<cards.length;i++){
-            for (int j=0;j<cards.length;j++){
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards.length; j++) {
                 System.out.print(cards[i][j]);
                 System.out.print(" , ");
             }
@@ -18,12 +17,12 @@ public class MagicalCardGameMain {
 
 
         magicalCardGameModel.setCardList(cards);
-        Scanner scanner = new Scanner(System. in);
-        String FirstShuffleColPos = scanner. nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String FirstShuffleColPos = scanner.nextLine();
         magicalCardGameModel.setFirstShuffleColPos(FirstShuffleColPos);
 
-        
+
 };
     }
 
-}
+
