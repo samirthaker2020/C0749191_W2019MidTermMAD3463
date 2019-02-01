d74c8f087757d6f4ab9c30a0f86ba5185b25aaa6
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-31 19:55:39 -0500

    modify the project

--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -1,12 +1,48 @@
 package com.lambton.magicalcardgame;
-
+import java.util.*;
 public class MagicalCardGameMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    public static void main (String args[])
-    {
-        MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
-        String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
-        magicalCardGameModel.setCardList(cards);
+        System.out.println("Enter The Number Of Matrix Rows");
 
+        int matrixRow = scan.nextInt();
+
+        System.out.println("Enter The Number Of Matrix Columns");
+
+        int matrixCol = scan.nextInt();
+
+
+        int[][] matrix = new int[matrixRow][matrixCol];
+
+        enterMatrixData(scan, matrix, matrixRow, matrixCol);
+
+
+        printMatrix(matrix, matrixRow, matrixCol);
+    }
+    public static void enterMatrixData(Scanner scan, int[][] matrix, int matrixRow, int matrixCol){
+        System.out.println("Enter Matrix Data");
+
+        for (int i = 0; i < matrixRow; i++)
+        {
+            for (int j = 0; j < matrixCol; j++)
+            {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix, int matrixRow, int matrixCol){
+        System.out.println("Your Matrix is : ");
+
+        for (int i = 0; i < matrixRow; i++)
+        {
+            for (int j = 0; j < matrixCol; j++)
+            {
+                System.out.print(matrix[i][j]+"\t");
+            }
+
+            System.out.println();
+        }
     }
 }
