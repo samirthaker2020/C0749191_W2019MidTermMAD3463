2a5f78f2f013a3985a897703bc52d51fe7681a82
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-31 19:02:58 -0500

    new  model class created and modification in project

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -3,15 +3,5 @@ package com.lambton.magicalcardgame;
 public class MagicalCardGameCalculator {
 
 
-        String
-                firstShuffleColPos;
-        String
-                secShuffleColPos;
-        String
-                [][] cardList = null;
-        String
-                [][] firstShuffle = null;
-        String
-                [][] secShuffle = null;
-        String guessedCard;
+
     }
---
+++ src/com/lambton/magicalcardgame/Model.java
@@ -0,0 +1,17 @@
+package com.lambton.magicalcardgame;
+
+public class Model {
+
+
+    String
+            firstShuffleColPos;
+    String
+            secShuffleColPos;
+    String
+            [][] cardList = null;
+    String
+            [][] firstShuffle = null;
+    String
+            [][] secShuffle = null;
+    String guessedCard;
+}
