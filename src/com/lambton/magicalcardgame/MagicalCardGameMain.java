a369edfcdde964d1e2d4760d4cb5f91581ffc4e3
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-31 20:10:05 -0500

    Made modification...still have bugs for shuffling array

--- src/com/lambton/magicalcardgame/MagicalCardGameMain.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameMain.java
@@ -5,9 +5,9 @@ public class MagicalCardGameMain {
     public static void main (String args[])
     {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
-        System.out.println("Welcome to Magical Card Game ..");
-        String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
-        System.out.println("Please select one card from below");
+       
+        String [][]cards = {{"1","9","k"},{"A","7","3"},{"3","6","D"}};
+        System.out.println("Please Select Any One::");
         for (int i=0;i<cards.length;i++){
             for (int j=0;j<cards.length;j++){
                 System.out.print(cards[i][j]);
