0257db04f2a5229f4b47500bf6475dd4bb043905
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-31 19:00:00 -0500

    Added getter & setter

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -3,4 +3,15 @@ package com.lambton.magicalcardgame;
 public class MagicalCardGameCalculator {
 
 
-}
+        String
+                firstShuffleColPos;
+        String
+                secShuffleColPos;
+        String
+                [][] cardList = null;
+        String
+                [][] firstShuffle = null;
+        String
+                [][] secShuffle = null;
+        String guessedCard;
+    }
